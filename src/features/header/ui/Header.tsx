f7712d202e3e01fbb7ba7bc13da02332774a3f73
Andrey Dremin <sankoa230@gmail.com>
2024-05-22 19:00:01 +0300

    feat: adaptive header component

--- src/features/header/ui/Header.tsx
+++ src/features/header/ui/Header.tsx
@@ -2,16 +2,19 @@ import { Container,useAppSelector } from "../../../shared/";
 import Logo from "../../../assets/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import {useUserQuery} from "../../../app"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "../../../shared/";
 import {} from "../../../app/"
 import { setAdmin, setAuth } from "../../../app/store/authSlice";
-
+import Burger from "../../../assets/burger.svg";
+import { MenuModal } from "../../../widget/menu";
 export const Header = () => {
 
   const {isAuth,isAdmin} = useAppSelector(state => state.auth)
 
   const {data,isSuccess} = useUserQuery(localStorage.getItem("userId"))
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const dispatch = useAppDispatch()
 
@@ -36,14 +39,19 @@ export const Header = () => {
     }
   },[isSuccess])
 
+  const handleOpenModal = () => setIsModalOpen(true);
+
+  const handleCloseModal = () => setIsModalOpen(false);
+
   return (
+    <>
     <header className="py-5">
       <Container>
         <nav className="flex justify-between">
           <Link to={"/"}>
             <img src={Logo} alt="logo rozum systems" className="basis-[25%]" />
           </Link>
-          <ul className="flex gap-3 items-center">
+          <ul className="flex gap-3 items-center max-md:hidden">
             <li className="border-[#7BBAF5] px-4 py-2 block text-[white] border-2 bg-[#7BBAF5] rounded-[10px]">
               <Link to={"/"}>
                 ГЛАВНАЯ
@@ -69,8 +77,17 @@ export const Header = () => {
               </>
             )}
           </ul>
+          <button onClick={handleOpenModal} className="md:hidden">
+            <img src={Burger} className="h-14 w-14" alt="" />
+          </button>
         </nav>
       </Container>
     </header>
+    <MenuModal
+        isOpen={isModalOpen}
+        onOpenModal={handleOpenModal}
+        onCloseModal={handleCloseModal}
+      />
+    </>
   );
 };
